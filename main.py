09c902287c7e9403ef733c34df7905a52a140b11
alejo0106 <Alejandroraeche@gmail.com>
2023-10-03 13:48:39 -0500

    update main.py
    added into the file an explanation of how to use np array and plt to build a graphic

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import cv2 as cv
-import numpy as py
+import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('./img/img1.jpeg')
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
+#crear graficos con matplotlib
+plt.title("Sports Watch Data"),plt.xlabel("Average Pulse"),plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.grid()
+
+'''
+#Trabajar con Imagenes
+img = cv.imread('./img/img1.jpeg')
 plt.imshow(img,'gray'), plt.title('Test 1')
+'''
 plt.show()
